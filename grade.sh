81a7ddc10325095f792fea3b015f9d65f169d36d
jakev220 <jrvillasenor@ucsd.edu>
2023-05-10 16:50:28 -0700

    Updated

--- grade.sh
+++ grade.sh
@@ -8,6 +8,20 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+set -e
+
+if [[ -f student-submission/ListExamples.java ]]
+then 
+    echo "Thank you for your submission!"
+    cp student-submission/ListExamples.java ./grading-area
+    cp ./TestListExamples.java ./grading-area
+    cp -r lib ./grading-area
+
+else
+    echo "Error: Please submit the correct file!"
+fi
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
